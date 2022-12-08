da2f9f19d3a359407c91ff23cd5b50e22d2ee726
Michal Sojka <michal.sojka@cvut.cz>
2022-12-08 23:00:48 +0100

    Add simple flake to make the results easily copyable between computers

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "Carla source build";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      packages.x86_64-linux.default = pkgs.callPackage ./carla-ue4.nix {};
+    };
+}
