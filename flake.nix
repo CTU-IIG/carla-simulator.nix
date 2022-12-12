53abecd0cb80a968ea48a586722d35621942b95b
Michal Sojka <michal.sojka@cvut.cz>
2022-12-12 14:56:26 +0100

    Add merged packages to the flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Carla source build";
+  description = "Carla simulator";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -7,6 +7,15 @@
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
-      packages.x86_64-linux.default = pkgs.callPackage ./carla-simulator/carla-ue4.nix {};
+      packages.x86_64-linux = rec {
+        ue4 = pkgs.callPackage ./carla-simulator/carla-ue4.nix {};
+
+        carla-client = pkgs.callPackage carla-client/carla_client.nix { inherit rpclib recast; };
+        carla-py = pkgs.python39.pkgs.callPackage carla-client/carla_py.nix { inherit carla-client rpclib recast osm2odr; };
+        #carla-ros-bridge = pkgs.callPackage carla-client/carla_ros_bridge.nix {};
+        osm2odr = pkgs.callPackage carla-client/osm2odr.nix {};
+        recast = pkgs.callPackage carla-client/recast.nix {};
+        rpclib = pkgs.callPackage carla-client/rpclib.nix {};
+      };
     };
 }
