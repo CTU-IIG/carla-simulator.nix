1b43d0034d9f72d037ff1ada54efd9b97558f9b8
Michal Sojka <michal.sojka@cvut.cz>
2022-12-12 14:00:20 +0100

    Move Nix expressions for build from source to a subdirectory

--- flake.nix
+++ flake.nix
@@ -7,6 +7,6 @@
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
-      packages.x86_64-linux.default = pkgs.callPackage ./carla-ue4.nix {};
+      packages.x86_64-linux.default = pkgs.callPackage ./carla-simulator/carla-ue4.nix {};
     };
 }
