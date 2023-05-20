b6ee81c9f0f067e554aa3998a883bad06b89000a
Michal Sojka <michal.sojka@cvut.cz>
2023-05-20 23:35:53 +0200

    Add carla-bin versions to flake.nix

--- flake.nix
+++ flake.nix
@@ -6,9 +6,14 @@
   outputs = { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      carla-bin-versions = import ./carla-bin/versions.nix { inherit pkgs; };
     in {
       packages.x86_64-linux = rec {
         ue4 = pkgs.callPackage ./carla-simulator/carla-ue4.nix {};
+
+        carla-bin_0_9_12 = carla-bin-versions."0.9.12";
+        carla-bin_0_9_13 = carla-bin-versions."0.9.13";
+        carla-bin_0_9_14 = carla-bin-versions."0.9.14";
 
         carla-client = pkgs.callPackage carla-client/carla-client/0.9.14.nix { inherit rpclib recast; };
         carla-py = pkgs.python3.pkgs.callPackage carla-client/carla-py/0.9.14.nix { inherit carla-client rpclib recast osm2odr; };
