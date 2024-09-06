22d541eb44ae264826bf34ee6c67a5920ecb3ac2
Michal Sojka <michal.sojka@cvut.cz>
2024-09-06 14:29:47 +0200

    Update llvmPackages
    llvmPackages_9 was removed from nixpkgs. CARLA works even with
    llvmPackages_12 so let's use this.

--- carla-bin/common.nix
+++ carla-bin/common.nix
@@ -7,7 +7,7 @@
 , libglvnd
 , libusb1
 , libxkbcommon
-, llvmPackages_9
+, llvmPackages_12
 , makeWrapper
 , patchelf
 , pigz
@@ -57,7 +57,7 @@ let
     buildInputs = [
       autoPatchelfHook
       makeWrapper
-      llvmPackages_9.openmp
+      llvmPackages_12.openmp
       libusb1
       zlib
     ];
